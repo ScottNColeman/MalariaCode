99b74b1461621d3c6bd3b5a5bd52481ecf1c850b
MattUppington <32743231+MattUppington@users.noreply.github.com>
2017-11-26 16:52:29 +0000

    Superfluous code removed

--- PartialDifferentialEquation.m
+++ PartialDifferentialEquation.m
@@ -16,25 +16,7 @@ s = linspace(0,t,M+1);
 new_u = zeros(size(u));
 
 % MATT - starting to include proper equations...
-uLeave = zeros(size(u));
-for     s1 = 1:size(uLeave,2)
-    for     j = 1:size(uLeave,3)
-        int_Pu_uTQu = 0;
-        for     s2 = 1:size(uLeave,2)
-            P = BuildMatrix(model,'P',s1,s2,0);
-            u_s2 = reshape(u(1,s2,:),size(u,3),1);
-            int_uTQu = 0;
-            for     s3 = 1:size(uLeave,2)
-                Q = BuildMatrix(model,'Q',s1,s2,s3);
-                u_s3 = reshape(u(1,s3,:),size(u,3),1);
-                int_uTQu = int_uTQu +transpose(u_s2)*Q(:,:,j)*u_s3;
-            end
-            int_Pu_uTQu = int_Pu_uTQu +P*u_s2 +int_uTQu;
-        end
-        uLeave(1,s1,j) = int_Pu_uTQu;
-    end
-end
-
+uLeave = DirectionalDerivative(model,u);
 % MATT - ...end of current edit section
 
 
