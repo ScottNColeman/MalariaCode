62979168bd897db52b5b22b6148e505ca7d0773e
James <jr15832@my.bristol.ac.uk>
2017-11-29 12:40:36 +0000

    Removed ResidenceTime functions from branch

--- PlotODEs.m
+++ PlotODEs.m
@@ -1,17 +1,21 @@
 function PlotODEs
 
 % Creates Timespace
-t = linspace(0,300,301);
+t = linspace(0,100,101);
 
 % Ratio of mosquitos to humans
 M = 1;
 
 % Set Rates
 r = 1/14;
+%r = 0.001;
 i = 1/20;
+%i = 0.00001;
 d = 0;
 c = 1/6;
-b = 1/2;
+%c = 0.002;
+%b = 1/2;
+b = 0.00002;
 
 % Set initial values
 x0 = [0.99;0.01;0;1*M;0*M];
@@ -30,9 +34,9 @@ opts = odeset('RelTol',1e-3, 'AbsTol' ,1e-6);
 figure;
 plot(t,x(:,1),'b-',t,x(:,2),'g-',t,x(:,3),'r-');
 hold on
-plot(t,ones(size(t))*x2(1),'b--')
-plot(t,ones(size(t))*x2(2),'g--')
-plot(t,ones(size(t))*x2(3),'r--')
+%plot(t,ones(size(t))*x2(1),'b--')
+%plot(t,ones(size(t))*x2(2),'g--')
+%plot(t,ones(size(t))*x2(3),'r--')
 xlabel('Time'); ylabel('Relative Population')
 legend('Sus','Inf','Dead')
 title('Humans')
@@ -41,8 +45,8 @@ title('Humans')
 figure;
 plot(t,x(:,4),'c-',t,x(:,5),'m-');
 hold on
-plot(t,ones(size(t))*x2(4),'c--')
-plot(t,ones(size(t))*x2(5),'m--')
+%plot(t,ones(size(t))*x2(4),'c--')
+%plot(t,ones(size(t))*x2(5),'m--')
 xlabel('Time'); ylabel('Relative Population')
 legend('Sus','Inf')
 title('Mosquitoes')
