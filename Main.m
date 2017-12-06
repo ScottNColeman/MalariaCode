b8ba92f0e1f461dbe2943b95c4f7b92a5b0f1cb5
Vladimir Macko <vlamacko@gmail.com>
2017-12-06 13:26:38 +0000

    Added publishable graph output, new print function, added .gitignore

--- Main.m
+++ Main.m
@@ -1,43 +1,46 @@
 % Initial matrices to store t and s values
-t = linspace(0,121,1201); % uses n index
+t = linspace(0, 200, 600); % uses n index
 s = t; % uses m index, half of these values are useless
 
 % MATT - current model iteration.
-model = 2;
+model = 1;
 
-i = 1/20;
-b = 1/2;
-r = 1/14;
-c = 1/6;
+i = 1 / 20;
+b = 1 / 2;
+r = 1 / 14;
+c = 1 / 6;
 
-Init = [(r*(b+c))/(b*(i+r));
-        (b*i-c*r)/(b*(i+r));
-        (c*(i+r))/(i*(b+c));
-        (b*i-c*r)/(i*(b+c))];
+Init = [(r * (b + c)) / (b * (i + r));
+        (b * i - c * r) / (b * (i + r));
+        (c * (i + r)) / (i * (b + c));
+        (b * i - c * r) / (i * (b + c))];
 
 % Initialise each class
-u = zeros(length(t),length(s),4);
+u = zeros(length(t), length(s), 4);
 
 % Initial conditions
-u(1,1,:) = [0.9,0.1,1,0]; % must be size I x 1
-%u(1,1,:) = [1,0,0.9,0.1];
-%u(1,1,:) = Init;
+% u(1,1,:) = [0.9, 0.1, 1, 0];
+u(1,1,:) = [1, 0, 0.9, 0.1];
+% u(1,1,:) = Init;
 disp(1)
 
 for n = 2:length(t)
-    u(:,n,:) = PartialDifferentialEquation( u(:,n-1,:), t(n), t(n)-t(n-1), model );%, t(n), t(n)-t(n-1));
+    u(:, n, :) = PartialDifferentialEquation(u(:, n-1, :), t(n), t(n) - t(n - 1), model);
     disp(n)
 end
 
-%%
+%% Plotting
+
 % Plot colour maps
 gap = 1; % If takes ages to plot, increase this to plot less values
 %st = transpose((0:gap:numSteps)*stepSize);
-for k = 1:size(u,3)
-    figure;
-    contourf(t(201:gap:end), s(201:gap:end), u(201:gap:end,201:gap:end,k))
-    colorbar
-    xlabel('Time'); ylabel('Residence Time')
+
+for k = 1:size(u, 3)
+    % figure;
+    % contourf(t(1:gap:end), s(1:gap:end), u(1:gap:end, 1:gap:end,k))
+    % colorbar
+    % xlabel('Time'); 
+    % ylabel('Residence Time')
     
     %figure;
     %test = log(1+u(:,:,k));
@@ -53,13 +56,42 @@ for k = 1:size(u,3)
     %plot(s,u(:,end-100,k))
     %sum(transpose(s).*u(:,end-100,k))/(sum(u(:,end-100,k)))
     
-    figure;
-    plot( t, sum(u(:,:,k),1) ) % total residence in each class against time
-    xlabel('Time'); ylabel('Population')
-    hold on
-    plot(t,Init(k)*ones(size(t)),'--')
-    %ylim([0,1])
 end
+
+% plot humans
+figure('Name', 'Humans');
+hold on;
+YLIM_MAX = 1.2;
+LINEWIDTH = 2;
+
+k = 1; % Suspectible humans
+plot(t, sum(u(:, :, k), 1), 'b-', 'LineWidth', LINEWIDTH) % total residence in each class against time
+plot(t, Init(k) * ones(size(t)), 'b--') % analytic equilibrium
+
+k = 2; % Infected humans
+plot(t, sum(u(:, :, k), 1), 'r-', 'LineWidth', LINEWIDTH) % total residence in each class against time
+plot(t, Init(k) * ones(size(t)), 'r--') % analytic equilibrium
+
+xlabel('Time'); ylabel('Population')
+legend('Susceptible', 'Susceptible steady state', 'Infected', 'Infected steady state')
+ylim([0, YLIM_MAX])
+
+% plot mosquitos 
+figure('Name', 'Mosquitos');
+hold on;
+
+k = 3; % Suspectible mosquitos
+plot(t, sum(u(:, :, k), 1), 'b-', 'LineWidth', LINEWIDTH) % total residence in each class against time
+plot(t, Init(k) * ones(size(t)), 'b--') % analytic equilibrium
+
+k = 4; % Infected mosquitoes 
+plot(t, sum(u(:, :, k), 1), 'r-', 'LineWidth', LINEWIDTH) % total residence in each class against time
+plot(t, Init(k) * ones(size(t)), 'r--') % analytic equilibrium
+
+xlabel('Time'); ylabel('Population')
+legend('Susceptible', 'Susceptible steady state', 'Infected', 'Infected steady state')
+ylim([0, YLIM_MAX])
+
 
 %%
 class = 1;
@@ -67,23 +99,23 @@ class = 1;
 switch class
     case 1
         % H1
-        a = (R*(B*I-C*R))/(B*(I+R));
-        b = (B*I-C*R)/(B+C);
+        a = (r * (b * i - c * r)) / (b * (i + r));
+        b = (r * i - c * r) / (b + c);
     case 2
         % H2
-        a = (R*(B*I-C*R))/(B*(I+R));
+        a = (r * (b * i - c * r)) / (b * (i + r));
         b = R;
     case 3
         % M1
-        a = (C*(B*I-C*R))/(I*(B+C));
-        b = (B*I-C*R)/(I+R);
+        a = (c * (b * i - c * r)) / (i * (b + c));
+        b = (b * i - c * r) / (i + r);
     case 4
         % M2
-        a = (C*(B*I-C*R))/(I*(B+C));
-        b = C;
+        a = (c * (b * i - c * r)) / (i * (b + c));
+        b = c;
 end
 
-u_pred = (s(2)-s(1))*a*(1-(s(2)-s(1))*b).^(0:length(s)-1);
+u_pred = (s(2) - s(1)) * a * (1 - (s(2) - s(1)) * b) .^ (0:length(s) - 1);
 
 %figure;
 %plot(s,u(:,end,class),'r-')
