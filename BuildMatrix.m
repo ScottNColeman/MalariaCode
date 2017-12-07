d320e0109faa20c30888ff5f0ed345730fbf1523
MattUppington <32743231+MattUppington@users.noreply.github.com>
2017-12-07 18:40:38 +0000

    Completed residence time functions

--- BuildMatrix.m
+++ BuildMatrix.m
@@ -47,11 +47,11 @@ switch  model
                 MAT(mInf,mInf) = c*(s1==s2);
             case    'Q'
                 %i = 0.00001;
-                i = 1/20;
+                infect = 1/20;
                 %b = 0.00002;
                 b = 1/2;
                 MAT = zeros(classes,classes,classes);
-                MAT(hSus,mInf,hSus) = i*(s1==s2);
+                MAT(hSus,mInf,hSus) = infect*(s1==s2);
                 %MAT(mInf,hSus,hSus) = i*(s1==s3);
                 MAT(mSus,hInf,mSus) = b*(s1==s2);
                 %MAT(mSus,hInf,mSus) = b*(s1==s3);
@@ -78,7 +78,7 @@ switch  model
         d = 1/2;
         r = 1/14;
         c = 1/6;
-        i = 1/20;
+        infect = 1/20;
         b = 1/2;
         switch matstr
             case    'uInit'
@@ -90,7 +90,7 @@ switch  model
                 MAT(mInf,mInf) = c*(s1==s2);
             case    'Q'
                 MAT = zeros(classes,classes,classes);
-                MAT(hSus,mInf,hSus) = i*(s1==s2);
+                MAT(hSus,mInf,hSus) = infect*(s1==s2);
                 MAT(mSus,hInf,mSus) = b*(s1==s2);
             case    'A'
                 MAT = zeros(classes,classes);
@@ -106,14 +106,13 @@ switch  model
         classes = 5;
         hSus = 1;
         hInf = 2;
-       
         hDead = 3;
         mSus = 4;
         mInf = 5;
         d = 1/2;
         r = 1/14;
         c = 1/6;
-        i = 1/20;
+        infect = 1/20;
         b = 1/2;
         switch matstr
             case    'uInit'
@@ -125,7 +124,7 @@ switch  model
                 MAT(mInf,mInf) = c*(s1==s2);
             case    'Q'
                 MAT = zeros(classes,classes,classes);
-                MAT(hSus,mInf,hSus) = i*(s1==s2);
+                MAT(hSus,mInf,hSus) = infect*(s1==s2);
                 MAT(mSus,hInf,mSus) = b*(s1==s2);
             case    'A'
                 MAT = zeros(classes,classes);
@@ -136,57 +135,89 @@ switch  model
                 MAT(mInf,mSus) = 1;
         end
     case    4
-        classes = 8;
+        classes = 10;
         hSus = 1;
         hInf = 2;
         hInfS = 3;
         hInfST = 4;
-        mJuv = 5;
-        mSus = 6;
-        mInc = 7; 
-        mInf = 8;
+        hDead = 5;
+        mJuv = 6;
+        mSus = 7;
+        mInc = 8;
+        mInf = 9;
+        mDead = 10;
         
         symDevInt = [8,30];
         recovInt = [7 28];
         worstDay = 21;
+        treatTime = 14;
         initRecov = 0.05;
-        initDeath = 0.05;
-        admit = 1/10;
-        
-        r=1/14;
-        c=1/6;
-        i = 1/20;
-        b = 1/2;
-        
-        
+        initDeathSym = 0.05;
+        initDeathTreat = 0.1;
+        admit = 0.1;
+        hBirth = 19/365000;
+        mBirth = 1/52;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CHECK
+        matureTime = 14;
+        bite = 1/2;
+        infect = 1/20;
+        mDeath = 1/52;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CHECK
+        incTime = 7;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CHECK
         switch  matstr
             case    'uInit'
                 MAT = zeros(1,s2,classes);
                 MAT(1,1,:) = reshape(s1,1,1,classes);
             case    'P'
-                MAT = zeros(classes);
-                MAT(hInf,hInf) = min(1,heaviside(s1-symDevInt(1))*... 
-                        (s1-symDevInt(1))/diff(symDevInt));
-                MAT(hInfS,hInfS) = 1-min(1-admit-initRecov-initDeath,...
+                MAT = zeros(classes,classes);
+                MAT(hInf,hInf) = (min(1,heaviside(s1-symDevInt(1))*... 
+                        (s1-symDevInt(1))/diff(symDevInt)))*(s1==s2);
+                MAT(hInfS,hInfS) = (1-min(1-admit-initRecov-initDeathSym,...
                         heaviside(recovInt(end)-s1)*...
-                        (1-admit-initRecov-initDeath)/diff(recovInt)*...
-                        (recovInt(end)-s1));
+                        (1-admit-initRecov-initDeathSym)/diff(recovInt)*...
+                        (recovInt(end)-s1)))*(s1==s2);
+                MAT(hInfST,hInfST) = (1-heaviside(treatTime-s1)*...
+                        (initDeathTreat/treatTime +1-initDeathTreat))*...
+                        (s1==s2);
+                MAT(mJuv,mSus) = heaviside(s1-matureTime)*(s1==s2);
+                MAT(mSus,mSus) = mDeath*(s1==s2);
+                MAT(mInc,mInc) = (mDeath +heaviside(s1-incTime)*...
+                        (1-mDeath))*(s1==s2);
+                MAT(mInf,mInf) = mDeath*(s1==s2);
             case    'Q'
                 MAT = zeros(classes,classes,classes);
-                MAT(hSus,mInf,hSus) = i*(s1==s2);
-            case    'A'
+                MAT(hSus,mInf,hSus) = infect*(s1==s2);
+                MAT(mSus,hInf,mSus) = bite*(s1==s2);
+                MAT(mSus,hInfS,mSus) = bite*(s1==s2);
+            case    'Ap'
                 MAT = zeros(classes,classes);
-                MAT(hSus,hInf) = 1;
                 MAT(hInf,hInfS) = 1;
-                scale = min(1-admit-initRecov-initDeath,...
+                hInfSscale = 1-min(1-admit-initRecov-initDeathSym,...
                         heaviside(recovInt(end)-s1)*...
-                        (1-admit-initRecov-initDeath)/diff(recovInt)*...
+                        (1-admit-initRecov-initDeathSym)/diff(recovInt)*...
                         (recovInt(end)-s1));
-                MAT(hInfS,hInfST) = 0.1/scale;
+                MAT(hInfS,hInfST) = admit/hInfSscale;
                 MAT(hInfS,hSus) = (initRecov +min(1-admit-initRecov,...
                         heaviside(s1-worstDay)*...
                         (1-admit-initRecov)/(recovInt(end)-worstDay)*...
-                        (s1-worstDay)))/scale;
+                        (s1-worstDay)))/hInfSscale;
+                MAT(hInfS,hDead) = 1/hInfSscale-sum(MAT(hInfS,:));
+                MAT(hInfST,hSus) = s1>=treatTime;
+                MAT(hInfST,hDead) = 1-sum(MAT(hInfST,:));
+                MAT(mJuv,mSus) = 1;
+                MAT(mSus,mDead) = 1;
+                MAT(mInc,mInf) = s1>=incTime;
+                MAT(mInc,mDead) = 1-sum(MAT(mInc,:));
+                MAT(mInf,mDead) = 1;
+            case    'Aq'
+                MAT = zeros(classes,classes);
+                MAT(hSus,hInf) = 1;
+                MAT(mSus,mInc) = 1;
+            case    'G'
+                MAT = zeros(classes,classes);
+                MAT(hSus,hSus) = hBirth;
+                MAT(hSus,hInf) = hBirth;
+                MAT(mJuv,mSus) = mBirth;
+                MAT(mJuv,mInc) = mBirth;
+                MAT(mJuv,mInf) = mBirth;
         end
         
 end
